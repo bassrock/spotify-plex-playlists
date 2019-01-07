df64e96a6217f6b9975de6d99666bb6482b2225e
Daniel Brooks <bass_rock@me.com>
2019-01-06 21:17:41 -0800

    Creating a proper readme

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 #Spotify -> Plex
+[![](https://img.shields.io/docker/automated/bassrock/spotify-plex-playlists.svg)](https://cloud.docker.com/repository/docker/bassrock/spotify-plex-playlists) [![](https://img.shields.io/docker/pulls/bassrock/spotify-plex-playlists.svg)](https://img.shields.io/docker/pulls/bassrock/spotify-plex-playlists.svg) [![](https://img.shields.io/docker/stars/bassrock/spotify-plex-playlists.svg)](https://cloud.docker.com/repository/docker/bassrock/spotify-plex-playlists)
 
 Converts Spotify URI's to Plex Playlists on a Specified Interval.
 
 ##Setup
-Use the docker
+Set the following Docker environment variables
 
-ENV SPOTIPY_CLIENT_ID ""
-ENV SPOTIPY_CLIENT_SECRET ""
-ENV PLEX_URL ""
-ENV PLEX_TOKEN ""
-ENV SPOTIFY_URIS ""
+`SPOTIPY_CLIENT_ID` - A Spotify client id created here: https://developer.spotify.com/dashboard/login
+
+`SPOTIPY_CLIENT_SECRET` - The Spotify client secret from the created client id
+
+`PLEX_URL` - Your plex url: `http://plex:32400`
+
+`PLEX_TOKEN` - Your plex token found by https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
+
+`SPOTIFY_URIS` - A comma seperated list of the spotify URI's you would like to import: `spotify:user:sonosplay,spotify:user:sonosplay:playlist:6nQjiSQhdf84s2AAxweRBv`
+
+`SECONDS_TO_WAIT` - How many seconds to wait before syncs
+
+The following URI's are supported:
+* A user's URI which will import all public playlists a user owns: `spotify:user:sonosplay`
+* A playlist URI which imports a specific playlist (must be public): `spotify:user:sonosplay:playlist:6nQjiSQhdf84s2AAxweRBv`
+
+Playlists will only be created on Plex if your Plex instance has at least one of the songs. Only songs found on your Plex will be created in the Plex Playlilst
--- spotify-sync.py
+++ spotify-sync.py
@@ -1,10 +1,9 @@
 import re
 import time
-
+import logging
 from plexapi.server import PlexServer
 from plexapi.audio import Track
 import spotipy
-import sys
 import os
 from spotipy.oauth2 import SpotifyClientCredentials
 from typing import List
@@ -61,7 +60,7 @@ def getPlexTracks(plex: PlexServer, spotifyTracks: []) -> List[Track]:
     plexTracks = []
     for spotifyTrack in spotifyTracks:
         track = spotifyTrack['track']
-        print("Searching Plex for: %s by %s" % (track['name'], track['artists'][0]['name']))
+        logging.info("Searching Plex for: %s by %s" % (track['name'], track['artists'][0]['name']))
 
         try:
             musicTracks = plex.search(track['name'], mediatype='track')
@@ -69,28 +68,28 @@ def getPlexTracks(plex: PlexServer, spotifyTracks: []) -> List[Track]:
             try:
                 musicTracks = plex.search(track['name'], mediatype='track')
             except:
-                print("Issue making plex request")
+                logging.info("Issue making plex request")
                 continue
 
         if len(musicTracks) > 0:
             plexMusic = filterPlexArray(musicTracks, track['name'], track['artists'][0]['name'])
             if len(plexMusic) > 0:
-                print("Found Plex Song: %s by %s" % (track['name'], track['artists'][0]['name']))
+                logging.info("Found Plex Song: %s by %s" % (track['name'], track['artists'][0]['name']))
                 plexTracks.append(plexMusic[0])
     return plexTracks
 
 
 def createPlaylist(plex: PlexServer, sp: spotipy.Spotify, playlist: []):
     playlistName = playlist['owner']['display_name'] + " - " + playlist['name']
-    print('Starting playlist %s' % playlistName)
+    logging.info('Starting playlist %s' % playlistName)
     plexTracks = getPlexTracks(plex, getSpotifyTracks(sp, playlist))
     if len(plexTracks) > 0:
         try:
             plexPlaylist = plex.playlist(playlistName)
-            print('Updating playlist %s' % playlistName)
+            logging.info('Updating playlist %s' % playlistName)
             plexPlaylist.addItems(plexTracks)
         except:
-            print("Creating playlist %s" % playlistName)
+            logging.info("Creating playlist %s" % playlistName)
             plex.createPlaylist(playlistName, plexTracks)
 
 def parseSpotifyURI(uriString: str) -> {}:
@@ -104,22 +103,26 @@ def parseSpotifyURI(uriString: str) -> {}:
 
 
 def runSync(plex : PlexServer, sp : spotipy.Spotify, spotifyURIs: []):
+    logging.info('Starting a Sync Operation')
     playlists = []
 
     for spotifyUriParts in spotifyURIs:
         if 'user' in spotifyUriParts.keys() and 'playlist' not in spotifyUriParts.keys():
+            logging.info('Getting playlists for %s' % spotifyUriParts['user'])
             playlists.extend(getSpotifyUserPlaylists(sp, spotifyUriParts['user']))
         elif 'user' in spotifyUriParts.keys() and 'playlist' in spotifyUriParts.keys():
+            logging.info('Getting playlist %s from user %s' % (spotifyUriParts['user'], spotifyUriParts['playlist']))
             playlists.append(getSpotifyPlaylist(sp, spotifyUriParts['user'], spotifyUriParts['playlist']))
 
     for playlist in playlists:
         createPlaylist(plex, sp, playlist)
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     spotifyUris = os.environ.get('SPOTIFY_URIS')
 
     if spotifyUris is None:
-        print("No spotify uris")
+        logging.error("No spotify uris")
 
     secondsToWait = int(os.environ.get('SECONDS_TO_WAIT', 1800))
     baseurl = os.environ.get('PLEX_URL')
